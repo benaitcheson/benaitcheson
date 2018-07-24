3e750bf824875ebf38b699d7d04a3b47a85703ce
Ben Aitcheson <benaitcheson@hotmail.com>
2018-07-24 19:47:26 +1000

    fixed whitespace

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+function whichElement(e) {
+    var targ;
+    if (!e) {
+        var e = window.event;
+    }
+    if (e.target) {
+        targ=e.target;
+    } else if (e.srcElement) {
+        targ=e.srcElement;
+    }    
+    alert("You clicked on a project.");
+}
+
+var today = new Date();
+var year = today.getFullYear();
+
+
+
+var el = document.getElementById('copyright');
+el.innerHTML = '<p>Copyright &copy; Ben Aitcheson' + year + '</p>';
